c08f32460f7bd7b4220d8681e79b50fa08cd3792
chaikovskaya <post-123@yandex.ru>
2021-09-20 17:40:01 +0300

    commit

--- js/team.js
+++ js/team.js
@@ -45,13 +45,54 @@ function initAjaxMoreTeam() {
     });
 }
 
+function initSliderTeamMore() {
+    $(".js-slider-team-more").each(function(){
+        var $element = $(this),
+            $list = $element.find('.js-slider-list'),
+            $item = $list.find('.js-slider-item');
+
+        var isStart = $item.length > 1 ? true : false;
+
+        $list.owlCarousel(jQuery.extend({}, GLOBAL.owl.common, {
+            loop: isStart,
+            mouseDrag: isStart,
+            touchDrag: isStart,
+            autoHeight: false,
+            smartSpeed: 500,
+            responsive: {
+                0: {
+                    items: 1,
+                    margin: 20,
+                },
+                720: {
+                    items: 3,
+                    margin: 30,
+                },
+                992: {
+                    items: 4,
+                    margin: 30,
+                },
+            },
+        }));
+        if (!isStart) {
+            $buttons.remove();
+        }
+    });
+}
+function reInitSliderTeamMore() {
+    $(".js-slider-team-more .js-slider-list").trigger('destroy.owl.carousel');
+}
+
 function initResizeWindowTeam() {
     if (window.matchMedia('(max-width: ' + GLOBAL.mobile + 'px)').matches) {
         initSliderTeam();
+        initSliderTeamMore();
     } else if (window.matchMedia('(max-width: ' + GLOBAL.tablet + 'px)').matches) {
         reInitSliderTeam();
+        initSliderTeamMore();
     } else {
         reInitSliderTeam();
+        reInitSliderTeamMore();
     }
 }
 
